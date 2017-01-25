4b8f64f15d7c6a57fe50ee8450328effb878fa40
Anter <anter@ghost.Home>
2017-01-25 00:49:16 +0000

    Added simple function run on startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <gtkmm.h>
 #include "mainwindow.h"
 
+void CheckUpdate()
+{
+	printf("No new update for AimTux\n");
+}
+
 int main(int argc, char *argv[])
 {
+	CheckUpdate();
+
 	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
 	MainWindow mainWindow;
