9a6533da56b4ce0d5992b7da75d0344de5a55dc2
nvllvs <nvllvs@users.noreply.github.com>
2017-01-26 16:46:11 +0100

    Clean up and add more debug messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <gtkmm.h>
 #include "gui/mainwindow.h"
+#include "operations/install.h"
 
 int main(int argc, char *argv[])
 {
+	Install::manager = Install::getPackageManager();
+
 	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
 	MainWindow mainWindow;
